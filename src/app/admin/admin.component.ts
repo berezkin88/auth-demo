5dc4e4e5460363de0bc270402ac54468666bc8a7
berezkin88 <berezkin05@gmail.com>
2020-10-07 23:34:19 +0300

    add authentication and guards

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  orders: any[];
+  orders: any;
 
   constructor(private orderService: OrderService) { }
 
